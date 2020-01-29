b8bb3cbca73799d89472d8b7317a7b0729a76d0d
TIA HWANG <56218426+TIAHWANG@users.noreply.github.com>
2020-01-29 16:58:01 +0900

    Update JS(if-statement)

--- Jan28 - Switch Alphabet/index.js
+++ Jan28 - Switch Alphabet/index.js
@@ -1,16 +1,25 @@
-const word = document.querySelector(".word");
-const button = document.querySelector(".switched");
-const result = document.querySelector(".js--result");
+const word = document.querySelector(".js-word");
+const button = document.querySelector(".js-btn");
+const result = document.querySelector(".js-result");
 
-function handleSubmit() {
+function handleClick() {
   const wordTyped = word.value;
-  const capital = wordTyped.match(/[A-Z]/g).length;
-  const small = wordTyped.match(/[a-z]/g).length;
-  if (capital > small) {
+  const capitalLetter = wordTyped.match(/[A-Z]/g);
+  const smallLetter = wordTyped.match(/[a-z]/g);
+
+  if (capitalLetter === null) {
     result.innerText = wordTyped.toUpperCase();
-  } else {
+  } else if (smallLetter === null) {
     result.innerText = wordTyped.toLowerCase();
+  } else {
+    const c = capitalLetter.length;
+    const s = smallLetter.length;
+    if (c >= s) {
+      result.innerText = wordTyped.toUpperCase();
+    } else {
+      result.innerText = wordTyped.toLowerCase();
+    }
   }
 }
 
-button.addEventListener("submit", handleSubmit);
+button.addEventListener("click", handleClick);
