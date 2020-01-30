5d1c1d71416894e6669084efa8ea7f6b430da00c
TIA HWANG <56218426+TIAHWANG@users.noreply.github.com>
2020-01-31 02:40:54 +0900

    Updated calculator JS

--- Jan27 - Simple Calculator/index.js
+++ Jan27 - Simple Calculator/index.js
@@ -6,28 +6,30 @@ const multiply = document.querySelector(".multiply");
 const divide = document.querySelector(".divide");
 const result = document.querySelector(".js--result");
 
-function handleSum() {
-  const num1 = parseFloat(firstNum.value);
-  const num2 = parseFloat(secondNum.value);
-  result.innerText = `The result is ${num1 + num2}`;
-}
-function handleSubtract() {
-  const num1 = firstNum.value;
-  const num2 = secondNum.value;
-  result.innerText = `The result is ${num1 - num2}`;
-}
-function handleMultiply() {
-  const num1 = firstNum.value;
-  const num2 = secondNum.value;
-  result.innerText = `The result is ${num1 * num2}`;
-}
-function handleDivide() {
-  const num1 = firstNum.value;
-  const num2 = secondNum.value;
-  result.innerText = `The result is ${num1 / num2}`;
-}
+const cal = {
+  sum: function() {
+    const num1 = parseInt(firstNum.value);
+    const num2 = parseInt(secondNum.value);
+    result.innerText = `The result is ${num1 + num2}`;
+  },
+  subtract: function() {
+    const num1 = firstNum.value;
+    const num2 = secondNum.value;
+    result.innerText = `The result is ${num1 - num2}`;
+  },
+  multiply: function() {
+    const num1 = firstNum.value;
+    const num2 = secondNum.value;
+    result.innerText = `The result is ${num1 * num2}`;
+  },
+  divide: function() {
+    const num1 = firstNum.value;
+    const num2 = secondNum.value;
+    result.innerText = `The result is ${num1 * num2}`;
+  }
+};
 
-sum.addEventListener("submit", handleSum);
-subtract.addEventListener("submit", handleSubtract);
-multiply.addEventListener("submit", handleMultiply);
-divide.addEventListener("submit", handleDivide);
+sum.addEventListener("click", cal.sum);
+subtract.addEventListener("click", cal.subtract);
+multiply.addEventListener("click", cal.multiply);
+divide.addEventListener("click", cal.divide);
--- README.md
+++ README.md
@@ -2,15 +2,17 @@
 
 ## 자바스크립트 연습
 
-- Challenge list: https://www.w3resource.com/javascript-exercises/
-- 매일 1개의 챌린지를 선택해서 코딩
+- Challenge list
+  - https://www.w3resource.com/javascript-exercises/
+  - https://www.codewars.com/
+- 매일 1개의 챌린지 코딩
 
 ## 목록
 
-- **Jan25 - Day and Time**: Write a JavaScript program to display the current day and time in the following format.
+- **Jan25 - Day and Time**: display the current day and time.
   <br>
-- **Jan27 - Simple Calculator**: Write a JavaScript program to calculate multiplication and division of two numbers (input from user).
+- **Jan27 - Simple Calculator**: calculate multiplication and division of two numbers (input from user).
   <br>
-- **Jan28 - Switch Alphabet**: Write a JavaScript program to switch case of the minimum possible number of letters to make a given string written in the upper case or in the lower case.
+- **Jan28 - Switch Alphabet**: switch case of the minimum possible number of letters to make a given string written in the upper case or in the lower case.
   <br>
-- **Jan29 - Add Alphabet**: Write a JavaScript program to create a new string from a given string with the first character of the given string added at the front and back.
+- **Jan29 - Add Alphabet**: create a new string from a given string with the first character of the given string added at the front and back.
